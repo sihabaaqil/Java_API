1918dd1aca8a008736b214cd8172dd90ab8528fe
aaqil <aaqilnas@gmail.com>
2024-06-06 23:14:05 +0800

    Joke API with getter & Setter

--- src/test/java/Demo/Joke.java
+++ src/test/java/Demo/Joke.java
@@ -1,4 +1,4 @@
-import com.fasterxml.jackson.annotation.JsonProperty;
+package Demo;
 
 public class Joke {
 
--- src/test/java/Demo/Joke_Main.java
+++ src/test/java/Demo/Joke_Main.java
@@ -1,3 +1,5 @@
+package Demo;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.HttpURLConnection;
