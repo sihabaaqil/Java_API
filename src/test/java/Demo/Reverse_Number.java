87c157f9306471947f1367980f55262c3ddd6810
Aaqil Sihab <aaqilnas@gmail.com>
2024-06-27 03:24:00 +0530

    Local dell
    dell

---
+++ src/test/java/Demo/Reverse_Number.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class Reverse_Number {
+
+	public static void main(String[] args) {
+		int n = 123456789;
+
+		int r = 0;
+		while (n != 0) {
+			int d = n % 10;
+			r = r * 10 + d;
+			n /= 10;
+
+		}
+
+		System.out.println(r);
+	}
+
+}
